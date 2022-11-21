776631d8403ccb8d250067261f9e92181deba1d3
Salooma <saloomaali2000@gmail.com>
2022-11-21 12:23:56 +0530

    menu is created

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Select the option \n 1 - Greatest of three numbers \n 2- Smallest of three numbers \n 3 - Is the given number is prime or not \n " +
+                "4 - Is the given number is odd or even \n 5 - Revers the given number \n 6 - Exit");
+         int n = input.nextInt();
+         if (n == 6){
+             System.out.println("You are exiting from the program");
+         }
+         else{
+             switch (n) {
 
 
-        System.out.println("Hello world!");
+             }
+             }
+         }
     }
 }
